e04c801fdb09ee9a9badbe67cc0b93d75d8f674f
Mhiracle <mhiracleebenezer@gmail.com>
2022-08-26 13:15:36 +0100

    Initial commit

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float delayTime = .5f;
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+       StartCoroutine(instantiateDog());
+    }
+
+    IEnumerator instantiateDog(){
+         yield return new WaitForSeconds(delayTime);
+         if (Input.GetKeyDown(KeyCode.Space))
+        {
+           
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
